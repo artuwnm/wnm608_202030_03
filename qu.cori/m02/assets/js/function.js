738cbb91a7c92f4efcbb6da060ce24f4f10872a5
coriqu <coriqco@gmail.com>
2020-03-07 23:06:25 -0800

    update

--- qu.cori/m02/assets/js/function.js
+++ qu.cori/m02/assets/js/function.js
@@ -1,22 +1,30 @@
 const myPhotos = {
-    
+
 }
+// let navBar = {
+//     init: function() {
+//         $.delegate($(".menu")[0], "a", "click", function(e) {
+//             let a = e.target;
+//             addClass(a, "active")
+//         });
+//     }
+// }
 let PhotoAlbum = {
 
     init: function() {
+        this.addSidebar();
         // console.log($(".img-container"));
         $.delegate($(".img-container")[0], "img", "click", function(e) {
             let a = e.target.parentNode;
-            console.log(a,a.className);
+            // console.log(a,a.className);
+
             if(a.className == "img-box active") {
-                $(".img-container")[0].className = "img-container";
+                removeClass( $('.img-container')[0],'open');
                 removeClass( $('.active')[0],'active');
-                // $('.img-container .img-box').removeClass('active');
-            } else {
-                $(".img-container")[0].className = "img-container open";
-                // removeClass( $('.img-container .img-box'),'active');
-                // $('.img-container .img-box').removeClass('active');
-                a.className = "img-box active";
+            } 
+            else {
+                addClass($(".img-container")[0], "open")
+                addClass(a, "active")
             }
             // $.delegate($(".open")[0], "div", "click", function(e) {
             //     console.log(e.target);
@@ -26,6 +34,9 @@ let PhotoAlbum = {
         $.delegate($("#left")[0], "div", "click", this.prevPhoto);
         $.delegate($("#right")[0], "div", "click", this.nextPhoto);
         $.delegate($("#middle")[0], "div", "click", this.showThumbnail);
+
+
+        $.delegate($("#photo-nav")[0], "li", "click", this.showCurrentPho);
     },
 
     readFromFolder: function() {
@@ -35,17 +46,18 @@ let PhotoAlbum = {
     nextPhoto: function() {
         console.log("right");
         let next = $(".active")[0].nextElementSibling;
-        console.log(next);
+        // console.log(next);
         if (next == null || next.className !== "img-box") {
             next = $('.img-box')[0];
         }
         removeClass( $('.active')[0],'active');
         addClass(next, "active")
     },
+
     prevPhoto: function() {
         console.log("left");
+
         let pre = $(".active")[0].previousElementSibling;
-        // console.log(pre);
         if (pre.className !== "img-box") {
             let imgBoxs = $('.img-box');
             let leng = imgBoxs.length;
@@ -54,17 +66,52 @@ let PhotoAlbum = {
         removeClass( $('.active')[0],'active');
         addClass(pre, "active")
     },
+
     showThumbnail: function() {
         console.log("middle");
-        $(".img-container")[0].className = "img-container";
+
+        removeClass( $('.img-container')[0],'open');
         removeClass( $('.active')[0],'active');
 
     },
-    addSidebar: function() {
+
+    showCurrentPho: function(e) {
+        let a = e.target;
+        console.log(a);
+        let photoNav = $('#photo-nav')[0];
+        if(photoNav.getElementsByClassName("active")[0]) {
+            removeClass( photoNav.getElementsByClassName("active")[0],'active');
+        }
+        addClass(a, "active");
+
+        console.log("showCurrentPho",e.target.value,a,'haha');
+
+        if ($('.active')[0]) {
+            removeClass( $('.active')[0],'active');
+        }
+        addClass($(".img-container")[0], "open")
+        addClass($(".img-box")[a.value], "active")
         
+
+    },
+
+    addSidebar: function() {
+        let imgBoxs = $('.img-box');
+        let leng = imgBoxs.length;
+        console.log(leng);
+        let ul = document.createElement('ul');
+        // let li = document.createElement('li');
+        for(let i=0; i<leng; i++) {
+            let li = document.createElement('li');
+            li.value = i;
+            ul.appendChild(li);
+        }
+
+        $('#photo-nav')[0].appendChild(ul);
     }
 }
 window.onload = function() {
+    // navBar.init();
     PhotoAlbum.init();
     // $.delegate(document.getElementById("list"), "li", "click", clickListener);
     // checkCookie();
