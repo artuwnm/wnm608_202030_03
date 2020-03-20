080a76cb46ffc8c03a3b0e0a5be204a160ad12fd
屠韵华 <tuyunhua@tuyunhuadeMacBook-Pro.local>
2020-03-20 00:58:42 -0600

    restructuring after midterm

---
+++ tu.yunhua/parts/meta.php
@@ -0,0 +1,10 @@
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
