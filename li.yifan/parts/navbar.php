515682e85999ca2d49fad42117ee4c416a0fb03f
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2020-03-22 23:53:04 -0700

    php files

---
+++ li.yifan/parts/navbar.php
@@ -0,0 +1,17 @@
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-stretch">
+				<h1>Store</h1>
+			</div>
+			<nav class="nav-flex flex-none">
+				<ul>
+					<!-- li*3>a[href=#]>{Link $} -->
+					<li class><a href="index.php">Home</a></li>
+					<li class><a href="product_list.php">Product</a></li>
+					<li class><a href="#">Cart</a></li>
+					<li class><a href="#">About</a></li>
+					<li class><a href="#">Log in</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
