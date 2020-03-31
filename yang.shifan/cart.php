d9c56565156fb573d76539f366390673929e01e9
syang46 <60861841+syang46@users.noreply.github.com>
2020-03-30 21:45:37 -0700

    make php tempates

---
+++ yang.shifan/cart.php
@@ -0,0 +1,2 @@
+<?php include('parts/header.php') ?>
+<?php include('parts/footer.php') ?>
---
+++ yang.shifan/index.php
@@ -0,0 +1,11 @@
+<?php include('parts/header.php') ?>
+<div class="view-window display-flex flex-align-center flex-justify-center" style="background-image:url(&#39;img/flower.jpg&#39;)">
+	<div class="card soft">
+		<h2>A heaven in A flower</h2>
+	</div>
+</div>
+
+<div class="heading">
+	<h1>A heaven in A flower</h1>
+</div>
+<?php include('parts/footer.php') ?>
