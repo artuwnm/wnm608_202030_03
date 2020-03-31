e204729b56a24c5bc906742cd914a77533812cca
Ariana A <aadili243@gmail.com>
2020-03-31 14:49:43 -0700

    revised wnm folder

---
+++ adili.ariana/parts/meta.php
@@ -0,0 +1,12 @@
+
+<meta charset="UTF-8">
+
+<base href="https://arianaad.com/aau/wnm608_03/adili.ariana/">
+
+<meta name="viewport" content="width=device-width">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
