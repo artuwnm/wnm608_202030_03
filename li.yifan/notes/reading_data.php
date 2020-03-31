4aa715d18fb335aec98378fd6a94d9f122e54edd
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2020-03-31 14:46:40 -0700

    module 8

---
+++ li.yifan/notes/reading_data.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Learning Data</title>
+	
+	<?php include "../parts/meta.php" ?>
+</head>
+<body>
+
+	<?php include "../parts/navbar.php" ?>
+
+	<div class="container">
+		<div class="card soft">
+		</div>
+	</div>
+	
+</body>
+</html>
