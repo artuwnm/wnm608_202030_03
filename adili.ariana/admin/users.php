dc77616817f9b78e2345a79100494ed76023bb00
Ariana A <aadili243@gmail.com>
2020-04-03 14:08:09 -0700

    user admin page

--- adili.ariana/admin/users.php
+++ adili.ariana/admin/users.php
@@ -65,6 +65,29 @@ HTML;
 		</div>
 	</header>
 
+
+<div class="container">
+	
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+	  Name: <input type="text" name="name" value="<?php echo $name;?>">
+	  <span class="error">* <?php echo $nameErr;?></span>
+	  <br><br>
+	  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+	  <span class="error">* <?php echo $emailErr;?></span>
+
+	  <br><br>
+	  <input type="submit" name="submit" value="Submit">  
+	</form>
+
+</div>
+
+
+
+
+
+
+
+
 	<div class="container">
 		<div class="card soft">
 
