5b0e22665edf91e1ce776c4ed4f4c537fba65ab8
潘方宁 <panfangning@panfangningdeMacBook-Pro.local>
2020-04-03 23:49:33 -0700

    update

---
+++ pan.frannie/24 Solar Terms/admin/users.php
@@ -0,0 +1,110 @@
+<?php
+
+include "../lib/php/functions.php";
+
+$users = getData("../data/users.json");
+
+// print_p($users);
+
+
+// file_put_contents, json_encode, explode, $_POST
+
+// CRUD - Create Read Update Delete
+
+
+function showUserPage($user) {
+
+$classes = implode(", ",$user->classes);
+
+echo <<<HTML
+<nav class="nav-crumbs">
+	<ul>
+		<li><a href="admin/users.php">Back</a></li>
+	</ul>
+</nav>
+<div>
+	<h2>$user->name</h2>
+	<div>
+		<strong>Type</strong>
+		<span>$user->type</span>
+	</div>
+	<div>
+		<strong>Email</strong>
+		<span>$user->email</span>
+	</div>
+	<div>
+		<strong>Classes</strong>
+		<span>$classes</span>
+	</div>
+</div>
+HTML;
+
+}
+
+
+
+
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Learning Data</title>
+	
+	<?php include "../parts/meta.php" ?>
+</head>
+<body>
+
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-stretch">
+				<h1>Admin</h1>
+			</div>
+			<nav class="nav-flex flex-none">
+				<ul>
+					<li><a href="admin/users.php">User List</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<div class="container">
+		<div class="card soft">
+
+			<?php 
+
+			if(isset($_GET['id'])){
+
+				showUserPage($users[$_GET['id']]);
+
+			} else {
+
+			?>
+			<h2>User List</h2>
+
+			<nav class="nav">
+			<ul>
+			<?php
+
+			foreach($users as $i=>$user) {
+				echo "
+				<li>
+					<a href='admin/users.php?id=$i'>$user->name</a>
+				</li>
+				";
+			}
+
+			?>
+			</ul>
+			</nav>
+
+			<?php 
+
+			}
+
+			?>
+		</div>
+	</div>
+	
+</body>
+</html>
---
+++ pan.frannie/24 Solar Terms/notes/reading_data.php
@@ -0,0 +1,56 @@
+<?php
+
+include "../lib/php/functions.php";
+
+$notes = getData("notes.json");
+$users = getData("../data/users.json");
+
+// print_p($users);
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Learning Data</title>
+	
+	<?php include "../parts/meta.php" ?>
+</head>
+<body>
+
+	<?php include "../parts/navbar.php" ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Notes</h2>
+
+			<ul>
+			<?php
+
+			for($i=0; $i<count($notes->notes); $i++){
+				echo "<li>{$notes->notes[$i]}</li>\n";
+			}
+
+			?>
+			</ul>
+		</div>
+		<div class="card soft">
+			<h2>Users</h2>
+
+			<ol>
+			<?php
+
+			for($i=0; $i<count($users); $i++){
+				echo "
+				<li>
+					<strong>{$users[$i]->name}</strong>
+					<span>[{$users[$i]->type}]</span>
+				</li>
+				";
+			}
+
+			?>
+			</ol>
+		</div>
+	</div>
+	
+</body>
+</html>
