72625c3d850802f237f59bffeb104ff5647096d1
Jane Choi <janechoi@Janes-iMac.local>
2020-04-04 17:56:45 -0700

    upload file

---
+++ Choi.Aejin/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<title>ABODE</title>
+    
+    <meta charset="UTF-8">
+
+    <base href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/">
+
+
+   <meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/lib/css/styleguide.css">
+	<link rel="stylesheet" href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/lib/css/gridsystem.css">
+	<link rel="stylesheet" href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/lib/css/storetheme.css">
+	 
+
+	   <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300|Source+Serif+Pro&display=swap" rel="stylesheet">  
+</head>
+<body>
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-stretch">
+				<a href="index.php"><img src="img/logo.jpg" alt="logo"></a>	
+				
+
+           </div>			
+
+           <nav class="nav-flex flex-none">
+
+                    <ul>
+					
+					<li><a href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/index.php">HOME</a></li>
+				    <li><a href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/About.php">ABOUT</a></li>
+                    <li><a href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/Product.php">PRODUCT</a></li>
+                    <li><a href="http://janechoidesign.com/aau/wnm608/Choi.Aejin/Cart.php">CART</a></li>
+						
+            </ul>
+			</nav>
+		</div>
+	</header>
+
+
+	<div class="view-window display-flex flex-align-center flex-justify-center" style="background-image:url('img/mainpic.jpg')">
+		<div class="card soft">
+		<h2>SHOP NOW!</h2>
+	</div>
+</div>
+
+
+
+<div class="container">
+	<h2>New Arrivals</h2>
+
+   <div class="display-flex">
+			<div class="flex-stretch">
+				<div class="card flat">
+
+
+		<img src="img/Tray.jpg" alt="Tray" style="width:100%">
+					<p>Product Name</p>
+					
+				</div>
+			</div>
+
+
+    <div class="flex-none" style="width: 1em"></div>
+			<div class="flex-stretch">
+				<div class="card flat">
+					<img src="img/Tray.jpg" alt="Tray" style="width:100%">
+					<p>Product Name</p>
+					
+				</div>
+			</div>
+
+
+   <div class="flex-none" style="width: 1em"></div>
+			<div class="flex-stretch">
+				<div class="card flat">
+					<img src="img/Tray.jpg" alt="Tray" style="width:100%">
+					<p>Product Name</p>
+					
+				</div>
+			</div>
+
+
+  </div>
+</div>
+
+
+<div class="container">
+	<h2>New Arrivals</h2>
+
+   <div class="display-flex">
+			<div class="flex-stretch">
+				<div class="card flat">
+
+
+		<img src="img/Tray.jpg" alt="cup" style="width:100%">
+					<p>Product Name</p>
+					
+				</div>
+			</div>
+
+
+    <div class="flex-none" style="width: 1em"></div>
+			<div class="flex-stretch">
+				<div class="card flat">
+					<img src="img/Tray.jpg" alt="Tray" style="width:100%">
+					<p>Product Name</p>
+					
+				</div>
+			</div>
+
+
+   <div class="flex-none" style="width: 1em"></div>
+			<div class="flex-stretch">
+				<div class="card flat">
+					<img src="img/Tray.jpg" alt="Tray" style="width:100%">
+					<p>Product Name</p>
+					
+				</div>
+			</div>
+
+
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
