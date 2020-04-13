6e96877d8823c02b346322ce29f9507c36819b09
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2020-04-12 21:13:04 -0700

    m8

--- li.yifan/notes/reading_data.php
+++ li.yifan/notes/reading_data.php
@@ -1,4 +1,13 @@
-<!DOCTYPE html>
+<?php
+
+include "../lib/php/functions.php";
+
+$notes = getData("notes.json");
+$users = getData("../data/users.json");
+
+// print_p($users);
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Learning Data</title>
@@ -11,8 +20,38 @@
 
 	<div class="container">
 		<div class="card soft">
+			<h2>Notes</h2>
+
+			<ul>
+			<?php
+
+			for($i=0; $i<count($notes->notes); $i++){
+				echo "<li>{$notes->notes[$i]}</li>\n";
+			}
+
+			?>
+			</ul>
+		</div>
+		<div class="card soft">
+			<h2>Users</h2>
+
+			<ol>
+			<?php
+
+			for($i=0; $i<count($users); $i++){
+				echo "
+				<li>
+					<strong>{$users[$i]->name}</strong>
+					<span>[{$users[$i]->type}]</span>
+				</li>
+				";
+			}
+
+			?>
+			</ol>
 		</div>
 	</div>
 	
 </body>
 </html>
+		
