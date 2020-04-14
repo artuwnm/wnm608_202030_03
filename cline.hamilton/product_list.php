fd084a1b1506bb3ec85d7f1fef999274b1555719
ham <ham@ham.com>
2020-04-14 14:47:10 -0700

    module 10

--- cline.hamilton/product_list.php
+++ cline.hamilton/product_list.php
@@ -1,4 +1,21 @@
-<!DOCTYPE html>
+<?php
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+
+$rows = getRows(
+	makeConn(),
+	"SELECT *
+	FROM `products`
+	ORDER BY `date_create` DESC
+	LIMIT 12
+	"
+);
+
+print_p($rows);
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Store: Product List</title>
@@ -10,16 +27,22 @@
 	<?php include "parts/navbar.php" ?>
 
 	<div class="container">
-		<div class="card soft">
-			<h2>Product List</h2>
+		<h2>Product List</h2>
 
-			<!-- ul>li*4>a[href="product_item.php"]>{Product $} -->
-			<ul>
-				<li><a href="product_item.php?id=1">Product 1</a></li>
-				<li><a href="product_item.php?id=2">Product 2</a></li>
-				<li><a href="product_item.php?id=3">Product 3</a></li>
-				<li><a href="product_item.php?id=4">Product 4</a></li>
-			</ul>
+		<div class="grid gap">
+
+		<?php
+
+
+
+		echo array_reduce(
+			$rows,
+			'productListTemplate'
+		);
+
+		?>
+
+
 		</div>
 	</div>
 	
