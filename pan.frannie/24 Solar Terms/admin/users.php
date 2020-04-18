5916651088d5506a1f0c818b7f1cf0d6151b02e7
潘方宁 <panfangning@panfangningdeMacBook-Pro.local>
2020-04-17 21:06:27 -0700

    update

--- pan.frannie/24 Solar Terms/admin/users.php
+++ pan.frannie/24 Solar Terms/admin/users.php
@@ -3,6 +3,7 @@
 include "../lib/php/functions.php";
 
 $users = getData("../data/users.json");
+$users = getData($filename);
 
 // print_p($users);
 
@@ -12,50 +13,134 @@ $users = getData("../data/users.json");
 // CRUD - Create Read Update Delete
 
 
+$empty_user = (object)[
+	"name"=>"",
+	"type"=>"",
+	"email"=>"",
+	"classes"=>[]
+];
+
+
+
+// LOGIC
+if(isset($_GET['action'])) {
+	switch($_GET['action']) {
+		case "update":
+			$users[$_GET['id']]->name = $_POST['user-name'];
+			$users[$_GET['id']]->type = $_POST['user-type'];
+			$users[$_GET['id']]->email = $_POST['user-email'];
+			$users[$_GET['id']]->classes = explode(", ",$_POST['user-classes']);
+
+			file_put_contents($filename,json_encode($users));
+
+			header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
+			break;
+		case "create":
+			$empty_user->name = $_POST['user-name'];
+			$empty_user->type = $_POST['user-type'];
+			$empty_user->email = $_POST['user-email'];
+			$empty_user->classes = explode(", ",$_POST['user-classes']);
+
+			$id = count($users);
+
+			$users[] = $empty_user;
+			// array_push($users,$empty_user);
+
+			file_put_contents($filename,json_encode($users));
+
+			header("location:{$_SERVER['PHP_SELF']}?id=$id");
+			break;
+		case "delete":
+			array_splice($users,$_GET['id'],1);
+
+			file_put_contents($filename,json_encode($users));
+
+			header("location:{$_SERVER['PHP_SELF']}");
+	}
+}
+
+
+
 function showUserPage($user) {
 
+
+
+$id = $_GET['id'];
 $classes = implode(", ",$user->classes);
+$addoredit = $id=="new" ? 'Add' : 'Edit';
+$createorupdate = $id=="new" ? 'create' : 'update';
+$deletebutton = $id=="new" ? '' : <<<HTML
+<li class="flex-none"><a href="{$_SERVER['PHP_SELF']}?id=$id&action=delete">Delete</a></li>
+HTML;
+
+$data_show = $id=="new" ? "" : <<<HTML
+<div class="card soft">
+<h2>$user->name</h2>
+<div class="form-control">
+	<strong>Type</strong>
+	<span>$user->type</span>
+</div>
+<div class="form-control">
+	<strong>Email</strong>
+	<span>$user->email</span>
+</div>
+<div class="form-control">
+	<strong>Classes</strong>
+	<span>$classes</span>
+</div>
+</div>
+HTML;
+
+
+
 
 echo <<<HTML
-<nav class="nav-crumbs">
+<nav class="nav-pills">
+	<div class="card soft">
 	<ul>
-		<li><a href="users.php">Back</a></li>
+		<li class="flex-none"><a href="{$_SERVER['PHP_SELF']}">Back</a></li>
+		<li class="flex-stretch"></li>
+		$deletebutton
 	</ul>
+	</div>
 </nav>
-
-
-
-<div>
-    <h2>$user->name</h2>
-        <div class="form-control">
-
-				<label class="form-label" for="input-example1">Type</label>
-				<input type="text1" class="form-input" placeholder="$user->type" id="input-example1">
-			</div>
-
+<form method="post" action="{$_SERVER['PHP_SELF']}?id=$id&action=$createorupdate">
+	<div class="grid gap">
+		<div class="col-xs-12 col-md-4">
+			$data_show
+		</div>
+		<div class="col-xs-12 col-md-8">
+			<div class="card soft">
+			<h2>$addoredit User</h2>
 			<div class="form-control">
-				<label class="form-label" for="input-example2">Email</label>
-				<input type="text1" class="form-input" placeholder="$user->email" id="input-example2">
+				<label class="form-lable" for="user-name">Name</label>
+				<input class="form-input" id="user-name" name="user-name" value="$user->name">
 			</div>
-
 			<div class="form-control">
-				<label class="form-label" for="input-example3">Classes</label>
-				<input type="text1" class="form-input" placeholder="$classes" id="input-example3">
-        </div>
-</div>
-
-
-
-
-
-<div class="btnstyle Submit">
-                <button class="btn first">Submit</button>
-</div>
-
-
+				<label class="form-lable" for="user-type">Type</label>
+				<input class="form-input" id="user-type" name="user-type" value="$user->type">
+			</div>
+			<div class="form-control">
+				<label class="form-lable" for="user-email">Email</label>
+				<input class="form-input" id="user-email" name="user-email" value="$user->email">
+			</div>
+			<div class="form-control">
+				<label class="form-lable" for="user-classes">Classes</label>
+				<input class="form-input" id="user-classes" name="user-classes" value="$classes">
+			</div>
+			<div class="form-control">
+				<input type="submit" class="form-button" value="Submit">
+			</div>
+			</div>
+		</div>
+	</div>
+</form>
 HTML;
 
 }
+
+
+
 
 
 
@@ -78,28 +163,34 @@ HTML;
 			<nav class="nav-flex flex-none">
 				<ul>
 					<li><a href="users.php">User List</a></li>
+					li><a href="users.php?id=new">Add New User</a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
 
 	<div class="container">
-		<div class="card soft">
+		
 
 			<?php 
 
 			if(isset($_GET['id'])){
 
-				showUserPage($users[$_GET['id']]);
+				showUserPage(
+					$_GET['id']=="new" ? 
+					$empty_user :
+					$users[$_GET['id']]
+				);
 
 			} else {
 
 			?>
+			<div class="card soft">
 			<h2>User List</h2>
             
 
 
-			<nav class="nav-user">
+			<nav class="nav">
 			<ul>
 			<?php
 
@@ -114,7 +205,7 @@ HTML;
 			?>
 			</ul>
 			</nav>
-
+            </div>
 
 			<?php 
 
@@ -128,7 +219,7 @@ HTML;
 
 
 
-		</div>
+		
 	</div>
 	
 </body>
