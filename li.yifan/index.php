f8dbed290b79d1dd264735793e9b584ca036819b
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2020-04-26 22:24:48 -0700

    M11

--- li.yifan/index.php
+++ li.yifan/index.php
@@ -7,30 +7,9 @@
 	<?php include "parts/meta.php" ?>
 </head>
 <body>
-	<!-- header.navbar>h1+nav.nav -->
-	<header class="navbar">
-		<div class="container display-flex">
-			
-			<div class="flex-stretch logo">
-				<img src="images/logo-b.png">
-			</div>
-			<nav class="nav flex-none">
-				<!-- ul>Li*3>a[href=#]>{Link $} -->
-				<ul class="display-flex">
-					<li class><a href="index.php">Home</a></li>
-					<li class><a href="product_list.php">Product</a></li>
-					<li class><a href="#">Cart</a></li>
-					<li class><a href="#">About</a></li>
-					<li class><a href="#">Log in</a></li>
-				</ul>
-			</nav>
-			<div class="nav flex-none">
-				<div class="form-control">
-					<input type="search" class="hotdog" placeholder="Search" />
-				</div>
-			</div>
-		</div>
-	</header>
+
+	<?php include "parts/navbar.php" ?>
+	
 
 	<section class>
 		<div class="topads">
--- li.yifan/parts/navbar.php
+++ li.yifan/parts/navbar.php
@@ -1,11 +1,10 @@
-<header class="navbar">
-		<div class="container display-flex">
+<!--		<div class="container display-flex">
 			<div class="flex-stretch">
 				<h1>Store</h1>
 			</div>
 			<nav class="nav-flex flex-none">
 				<ul>
-					<!-- li*3>a[href=#]>{Link $} -->
+					-- li*3>a[href=#]>{Link $} --
 					<li class><a href="index.php">Home</a></li>
 					<li class><a href="product_list.php">Product</a></li>
 					<li class><a href="#">Cart</a></li>
@@ -13,5 +12,30 @@
 					<li class><a href="#">Log in</a></li>
 				</ul>
 			</nav>
+		</div>
+	</header> -->
+
+	<!-- header.navbar>h1+nav.nav -->
+	<header class="navbar">
+		<div class="container display-flex">
+			
+			<div class="flex-stretch logo">
+				<img src="images/logo-b.png">
+			</div>
+			<nav class="nav flex-none">
+				<!-- ul>Li*3>a[href=#]>{Link $} -->
+				<ul class="display-flex">
+					<li class><a href="index.php">Home</a></li>
+					<li class><a href="product_list.php">Product</a></li>
+					<li class><a href="#">Cart</a></li>
+					<li class><a href="#">About</a></li>
+					<li class><a href="#">Log in</a></li>
+				</ul>
+			</nav>
+			<div class="nav flex-none">
+				<div class="form-control">
+					<input type="search" class="hotdog" placeholder="Search" />
+				</div>
+			</div>
 		</div>
 	</header>
