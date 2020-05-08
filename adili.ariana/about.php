65dd53aaa5b42e5fb210333229e29723e20f79c8
Ariana A <aadili243@gmail.com>
2020-05-08 12:46:41 -0700

    revised about

--- adili.ariana/about.php
+++ adili.ariana/about.php
@@ -13,6 +13,7 @@
 			<div class="card soft">
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, illum magni dolor aspernatur vero repudiandae porro ab accusamus nostrum, quibusdam, possimus vel dolorem nemo eos exercitationem. Impedit laboriosam commodi, at.</p>
 			<p>Sed atque, molestiae aperiam voluptatum deleniti quis amet mollitia! Aliquam amet eius molestias dignissimos nesciunt tempore voluptate sunt voluptatibus praesentium, aut deleniti animi officiis ipsa quam unde, similique quasi adipisci.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae aperiam numquam sit optio dolorem, consequuntur facere asperiores labore enim sunt.</p>
 		</div>
 	</div>
 	
