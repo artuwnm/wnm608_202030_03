d60420b210b56425cd8c1d689e2fe68a9c52d536
Ariana A <aadili243@gmail.com>
2020-05-08 12:52:55 -0700

    revised product cart

--- adili.ariana/product_cart.php
+++ adili.ariana/product_cart.php
@@ -11,11 +11,11 @@ $cartItems = []; //getCartItems()
 <head>
 	<title>Begari | Product Cart</title>
 	
-	<?php include "/parts/meta.php" ?>
+	<?php include __DIR__. "/parts/meta.php" ?>
 </head>
 <body>
 
-	<?php include "/parts/navbar.php" ?>
+	<?php include __DIR__. "/parts/navbar.php" ?>
 
 	<div class="container">
 		<nav class="nav-crumbs" style="margin:1em 0">
