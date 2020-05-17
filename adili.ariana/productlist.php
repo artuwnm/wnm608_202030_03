6d97f8b4677b06d90a487a72d84e2cdd54d36960
Ariana A <aadili243@gmail.com>
2020-05-17 13:31:29 -0700

    about and cart

--- adili.ariana/productlist.php
+++ adili.ariana/productlist.php
@@ -6,7 +6,7 @@ return $r.<<<HTML
 <div class="itemlist-item display-flex">
 	<div class="flex-none">
 		<div class="image-square">
-			<img src="/images/store/$o->thumbnail">
+			<img src="$o->thumbnail">
 		</div>
 	</div>
 	<div class="flex-stretch">
