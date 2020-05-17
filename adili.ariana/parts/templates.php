aa023256064f44d3495cc4f72608d2a54419b57d
Ariana A <aadili243@gmail.com>
2020-05-17 14:36:43 -0700

    templates and cart

--- adili.ariana/parts/templates.php
+++ adili.ariana/parts/templates.php
@@ -24,7 +24,7 @@ $selectamount = selectAmount($o->amount,10);
 return $r.<<<HTML
 <div class="display-flex card-section">
 	<div class="flex-none product-thumbs" style="margin-right:1em">
-		<img src="/images/store/$o->thumbnail">
+		<img src="$o->thumbnail">
 	</div>
 	<div class="flex-stretch">
 		<div class="display-flex">
