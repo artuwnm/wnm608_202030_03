0e946322df9a64171bcfdb00099a6c38b999cc88
janelleweiwei <60762961+janelleweiwei@users.noreply.github.com>
2020-05-17 18:57:44 -0700

    add bag count UI

--- li.zhuwei/store/parts/navbar.php
+++ li.zhuwei/store/parts/navbar.php
@@ -1,3 +1,19 @@
+<?php
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+
+function get_cart_items() {
+  $numItems = count(getCartItems());
+  if ($numItems > 0) {
+    echo "($numItems)";
+  }
+}
+
+?>
+
+
 <header class="navbar" style="background-color: #283b54; color:white;">
   <div class="container display-flex">
     <div class="flex-stretch" onclick="window.location.replace('./index.php');">
@@ -12,7 +28,9 @@
         <li><a href="collection.php">Collection</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="login.php">Log In <i class='far fa-address-book' style='color:#f3cfd5'></i></i></a></li>
-          <a href="bag.php">Bag
+          <a href="bag.php">
+            <span>Bag</span>
+            <span class="badge"><?php get_cart_items() ?></span>
             <i class="fas fa-shopping-bag"></i>
           </a>
         </li>
