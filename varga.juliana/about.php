3f43cd6de393759e7dc6b0065ac20f43f6895ac1
Juli Varga <julimunoz@Julianas-MacBook-Pro.local>
2020-05-18 23:46:14 -0700

    Final Read me

--- varga.juliana/about.php
+++ varga.juliana/about.php
@@ -23,9 +23,10 @@
 	</div>
 <br>
 			<div>
-				<p style="color: grey;text-align: center;">We are a well-established jewelry store in San Francisco, CA.
-				that has grown with our community from a<br>small store to a multiple brand jewelry retailer. As a family business, we take pride in our work,<br>from the jewelers bench to the showroom.
-				All our diamond jewelry is hand selected from<br>thousands of
+				<p style="color: grey"> We are a well-established jewelry store in San Francisco, CA.
+				that has grown with our community from a small store to a multiple brand jewelry retailer.
+				<br>As a family business, we take pride in our work, from the jewelers bench to the showroom.
+				All our diamond jewelry is hand selected from thousands of
 				styles and is guaranteed to be the best quality for the price.</p>
 			</div>
 		</div>
